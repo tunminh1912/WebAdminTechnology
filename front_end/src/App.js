63ed01ea5c5559b48a9ed5d453c45b63e3d183f4
0n1ch4nb4k4 <2254810193@vaa.edu.vn>
2024-11-13 14:48:28 +0700

    Update App.js login register profile

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage'; 
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
 import './styles/global.css';  
 import Product from './components/category_details';
 import Productdetails from './components/product_details';
@@ -14,6 +17,9 @@ function App() {
         <Route path="/products" element={<Product />} />
         <Route path="/products/:id" element={<Productdetails />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
         </Routes>
     </Router>
   );
