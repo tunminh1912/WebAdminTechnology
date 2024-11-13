9070e85230fb6d1cc497e8dbd7472d7537215773
0n1ch4nb4k4 <2254810193@vaa.edu.vn>
2024-11-14 00:45:17 +0700

    Update user.js import jsonwebtoken

--- API/routers/user.js
+++ API/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 const User = require('../Model/user');
 
 const user_router = express.Router();
