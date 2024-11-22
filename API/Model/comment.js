277972b0b13d670f1bac2ae7c8e248eba2cef7c4
mydayoanhh <163551598+mydayoanhh@users.noreply.github.com>
2024-11-23 00:22:43 +0700

    Update comment.js

--- API/Model/comment.js
+++ API/Model/comment.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 const RateSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  comment: { type: String, required: true },
-  rating: { type: Number, required: true, min: 1, max: 5 },
-  createdAt: { type: Date, default: Date.now }
+  users: [{
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    comment: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    createdAt: { type: Date, default: Date.now }
+  }],
 });
 
 module.exports = mongoose.model('Rate', RateSchema);
