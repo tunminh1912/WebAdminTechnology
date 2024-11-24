8a86b358dc90bb5a7d07e36e307760d5e0418760
mydayoanhh <163551598+mydayoanhh@users.noreply.github.com>
2024-11-24 20:03:15 +0700

    Update product_details.jsx

--- front_end/src/components/product_details.jsx
+++ front_end/src/components/product_details.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Header from "./Navbar";
 import CommentSection from './CommentSection';
+import Grid from '@mui/material/Grid';
 
 function Productdetails() {
     const { id } = useParams();
